5ec08a9116a5cab5af0759c52ededc4c4829d5ab
Gutemberg <gutembergsouzadejesus@gmail.com>
2021-09-06 16:40:47 -0300

    correçao fale conosco

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,8 @@ const express       = require('express')
 const routes        = express.Router()
 
 routes.get('/', (req, res) => res.render("index"))
+routes.get('/aboutus', (req, res) => res.render("index"))
+routes.get('/blog', (req, res) => res.render("index"))
+routes.get('/blog_detalhes', (req, res) => res.render("index"))
 
 module.exports = routes
